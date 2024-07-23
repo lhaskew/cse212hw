e33afd3fa4895d0bc27fa935bbfded27b917caec
Lucy <has21013@byui.edu>
2024-07-22 21:13:08 -0600

    changes

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -8,6 +8,9 @@ public class Node {
     }
 
     public void Insert(int value) {
+        if (value == Data) {
+            return;
+        }
         if (value < Data) {
             // Insert to the left
             if (Left is null)
@@ -24,13 +27,23 @@ public class Node {
         }
     }
 
+
     public bool Contains(int value) {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data) {
+            return true;
+        }
+        if (value < Data) {
+            return Left?.Contains(value) ?? false;
+        }
+        else {
+            return Right?.Contains(value) ?? false;
+        }
     }
 
+
     public int GetHeight() {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
